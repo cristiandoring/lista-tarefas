2faf37c3022787df989ea93fa9084e79cd486c69
cristiandoring <cristiandoring@gmail.com>
2024-12-06 13:49:51 -0300

    deletarTarefa

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 let entrada = document.getElementById('entrada-tarefa')
-
+let contador = 0
 let botaoAdicionar  = document.getElementById('adicionar-tarefa')
 let main = document.getElementById("lista-tarefas")
 
 function adicionarTarefa(){
     let textoEntrada = entrada.value
-
+    
     if((textoEntrada !== "") && (textoEntrada !== null) && ( textoEntrada !== undefined)){
-        let novoItem = `<div class="item">
+
+        ++contador
+        let novoItem = `<div id="${contador}" class="item">
             <section class="item-icone">
-                <i class="icon-cicle"> <img src="media/circle-outline.svg" alt="circle"></i>
+                <i class="icon-cicle"> <img src="media/circle-outline.svg" alt="circle" onclick="checkTarefa()"></i>
             </section>
             <section class="item-nome">
                 ${textoEntrada}
             </section>
             <section class="item-botao">
-                <button class="delete"> </button>
+                <button class="delete" onclick="deletarTarefa(${contador})" </button>
             </section>
         </div>`;
 
@@ -26,6 +28,26 @@ function adicionarTarefa(){
         entrada.focus()
     }
 }
+
+function deletarTarefa(id){
+    var tarefa = document.getElementById(id)
+    tarefa.remove()
+}
+
+function checkTarefa(){
+    `<div class="item-clicado">
+            <section class="item-icone">
+                <i class="icon-cicle-check"> <img src="media/check-circle-outline.svg" alt="circle"></i>
+            </section>
+            <section class="item-nome">
+                ${textoEntrada}
+            </section>
+            <section class="item-botao">
+                <button class="delete"> </button>
+            </section>
+        </div>`
+}
+
 
 //aciona o botão adicionar quando aperto Enter no teclado 
 entrada.addEventListener("keyup", function(event){
